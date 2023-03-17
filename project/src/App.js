332b3a0228b84c120147f5d244f020d18e12799e
NicoB <nicobasma_@hotmail.com>
2023-03-17 20:45:28 +0000

    adding

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
+import Hero from './components/Hero';
 
 function App() {
   return (
+    <>
     <Navbar />
+    <Hero/>
+    </>
   );
 }
 
