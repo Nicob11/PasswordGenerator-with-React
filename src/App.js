a174de113eaa1d0c00e8a5217615808bd38f5364
NicoB <nicobasma_@hotmail.com>
2023-03-20 20:12:33 +0000

    checked variable

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Checkbox from './components/CheckBox';
 
 function App() {
   return (
     <>
     <Navbar />
     <Hero/>
+    <Checkbox/>
     </>
   );
 }
---
+++ src/components/CheckBox.jsx
@@ -0,0 +1,38 @@
+import React,{ useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { Container, FormText, Button } from 'react-bootstrap';
+import encode from '../utils/functions';
+
+
+function Checkbox() {
+
+    const [checked, setCheked] = useState(false);
+
+    const handleChecked = (event) => {
+        setCheked(event.target.checked);
+      }
+
+    return (
+      <>
+      <Container>
+        <h1>Selecciona una o varias opciones</h1>
+        <InputGroup className="m-3">
+          <InputGroup.Checkbox aria-label="Checkbox for following text input" onChange={handleChecked}/>
+          <FormText>Incluir Mayusculas</FormText>
+        </InputGroup>
+        <InputGroup className="m-3">
+          <InputGroup.Checkbox aria-label="Checkbox for following text input" onClick={handleChecked} />
+          <Form.Text aria-label="Text input with checkbox">Incluir Numeros</Form.Text>
+        </InputGroup>
+        <InputGroup className="m-3">
+          <InputGroup.Checkbox aria-label="Checkbox for following text input" onClick={handleChecked} />
+          <Form.Text aria-label="Text input with checkbox">Incluir Simbolos</Form.Text>
+        </InputGroup>
+        <Button>Generate Password</Button>
+        </Container>
+      </>
+    );
+  }
+  
+  export default Checkbox;
